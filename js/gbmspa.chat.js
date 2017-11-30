50490acffaf5496c991e5938f42464ae53e29ddc
mnoesen <mnoesen@users.noreply.github.com>
2017-11-30 16:32:35 -0600

    Modify gbmspa.chat.js to end of module scp vars, p121

--- js/gbmspa.chat.js
+++ js/gbmspa.chat.js
@@ -9,22 +9,68 @@
 	regexp : true, sloppy : true, vars     : false, 
 	white  : true
 */
-/*global $, gbmspa */
+/*global $, gbmspa, getComputedStyle */
 
 gbmspa.chat = (function () {
 //-------- BEGIN MODULE SCOPE VARIABLES --------
 var 
 	configMap = {
 		main_html : String()
-			+ '<div style="padding:1em; color:#fff;">'
+			/*+ '<div style="padding:1em; color:#fff;">'
 			+ 'Submit a request to building maintenance'
-			+ '</div>',
-			settable_map : {},
+			+ '</div>', */
+			+ '<div class="gbmspa-chat">'
+			   + '<div class="gbmspa-chat-head">'
+			   + '<div class="gbmspa-chat-head-toggle">+</div>'
+			   + '<div class="gbmspa-chat-head-title">'
+			     + 'Chat'
+			   + '<div>'  
+			  + '<div>'
+			  + '<div class="gbmspa-chat-closer">x</div>'
+			  + '<div class="gbmspa-chat-sizer">'
+			    + '<div class="gbmspa-chat-msgs"></div>' 
+			    + '<div class="gbmspa-chat-box">'
+			      + '<input type="text"/>'
+			      + '<div>send</div>'
+			    + '</div>' 
+			  + '</div>'
+	    + '</div>',
+			settable_map : {
+				slider_open_time    : true, 
+				slider_close_time   : true, 
+				slider_opened_em    : true, 
+				slider_closed_em    : true, 
+				slider_opened_title : true, 
+				slider_closed_title : true, 
+
+				chat_model      : true, 
+				people_model    : true, 
+				set_chat_anchor : true, 
+			},
+
+			slider_open_time : 250,
+			slider_close_time : 250, 
+			slider_opened_em : 16, 
+			slider_closed_em : 2,
+			slider_opened_title : 'Click to close',
+			slider_closed_title : 'Click to open',
+
+			chat_model : null, 
+			people_model : null, 
+			set_chat_anchor : null
 	},
-	stateMap = { $container : null },
+	stateMap = { 
+		$append_target   : null,
+		position_type    : 'closed',
+		px_per_em        : 0,
+		slider_hidden_px : 0, 
+		slider_closed_px : 0, 
+		slider_opened_px : 0
+	},	
 	jqueryMap = {},
 
-	setJqueryMap, configModule, initModule
+	setJqueryMap, getEmSize, setPxSizes, setSliderPosition, 
+	onClickToggle, configModule, initModule
 	;
 //------- END MODULE SCOPE VARIABLES ----------
 
