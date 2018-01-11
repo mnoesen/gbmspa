1a9c846633c1af2e2fa78b2e8b4f906022989567
mnoesen <mnoesen@users.noreply.github.com>
2018-01-11 11:46:20 -0600

    Added note to gbmspa.js directing readers to the shell for the meat of the app.

--- js/gbmspa.js
+++ js/gbmspa.js
@@ -3,6 +3,10 @@
  * Root namegbmspace module
 */
 
+//NOTE: There is an MVC style framework to this web app.
+//  Most of the code one might exepct to find here 
+//  can be found in gbmspa.shell.js (referred to as "the shell").
+
 /*jslint           browser : true,   continue : true,
   devel  : true,    indent : 2,       maxerr  : 50,
   newcap : true,     nomen : true,   plusplus : true,
